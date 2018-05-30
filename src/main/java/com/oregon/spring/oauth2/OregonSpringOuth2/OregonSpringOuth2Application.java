de22c3305979e5b6dabe198405ea18213038f686
u206867 <vimal.radhakrishnan@suncorp.com.au>
2018-05-30 16:01:40 +1000

    Initial commit

---
+++ src/main/java/com/oregon/spring/oauth2/OregonSpringOuth2/OregonSpringOuth2Application.java
@@ -0,0 +1,12 @@
+package com.oregon.spring.oauth2.OregonSpringOuth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OregonSpringOuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OregonSpringOuth2Application.class, args);
+	}
+}
